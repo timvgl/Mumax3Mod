e8153ad127947e007657b69178d83c792fb25985
timvgl <tim.vogel@physik.uni-kl.de>
2025-02-10 14:48:15 +0100

    support string mmx code execution

--- cmd/mumax3/main.go
+++ cmd/mumax3/main.go
@@ -21,15 +21,16 @@ import (
 )
 
 var (
-	flag_failfast = flag.Bool("failfast", false, "If one simulation fails, stop entire batch immediately")
-	flag_test     = flag.Bool("test", false, "Cuda test (internal)")
-	flag_version  = flag.Bool("v", true, "Print version")
-	flag_vet      = flag.Bool("vet", false, "Check input files for errors, but don't run them")
-	flag_template = flag.Bool("template", false, "use template method from amumax")
-	flag_flat     = flag.Bool("flat", false, "flat structure for template")
-	flag_pipeline = flag.Int("pipelineLength", 1, "")
-	flag_encapsle = flag.Bool("encapsle", false, "")
-	flag_example  = flag.Bool("example", false, "")
+	flag_failfast   = flag.Bool("failfast", false, "If one simulation fails, stop entire batch immediately")
+	flag_test       = flag.Bool("test", false, "Cuda test (internal)")
+	flag_version    = flag.Bool("v", true, "Print version")
+	flag_vet        = flag.Bool("vet", false, "Check input files for errors, but don't run them")
+	flag_template   = flag.Bool("template", false, "use template method from amumax")
+	flag_flat       = flag.Bool("flat", false, "flat structure for template")
+	flag_pipeline   = flag.Int("pipelineLength", 1, "")
+	flag_encapsle   = flag.Bool("encapsle", false, "")
+	flag_example    = flag.Bool("example", false, "")
+	flag_stringCode = flag.String("c", "", "")
 	// more flags in engine/gofiles.go
 )
 
@@ -59,7 +60,13 @@ func main() {
 
 	defer engine.Close() // flushes pending output, if any
 
-	if *flag_template {
+	stringMode := false
+	if *flag_stringCode != "" && flag.NArg() == 0 {
+		runStringAndServe(*flag_stringCode)
+		stringMode = true
+	}
+
+	if *flag_template && !stringMode {
 		args := flag.Args()
 		if len(args) > *flag_pipeline {
 			panic(fmt.Sprintf("Found unparsed args.\n %v", args))
@@ -100,7 +107,7 @@ func main() {
 		}
 		wg.Wait()
 		os.Exit(int(exitStatus))
-	} else {
+	} else if !*flag_template && !stringMode {
 		switch flag.NArg() {
 		case 0:
 			if *engine.Flag_interactive {
@@ -151,6 +158,54 @@ func runFileAndServe(fname string) {
 		runScript(fname)
 	}
 	engine.WaitFFTs4DDone()
+}
+
+func runStringAndServe(script string) {
+	runScriptString(script)
+	engine.WaitFFTs4DDone()
+}
+
+func runScriptString(scriptStr string) {
+	fname := "script.mx3"
+	err := os.WriteFile(fname, []byte(scriptStr), 0644)
+	if err != nil {
+		panic(err)
+	}
+	outDir := util.NoExt(fname) + ".out"
+	if *engine.Flag_od != "" {
+		outDir = *engine.Flag_od
+	}
+	engine.InitIO(fname, outDir, *engine.Flag_forceclean)
+
+	fname = engine.InputFile
+
+	var code *script.BlockStmt
+	var err2 error
+	if fname != "" {
+		// first we compile the entire file into an executable tree
+		code, err2 = engine.CompileFile(fname)
+		util.FatalErr(err2)
+	}
+
+	// now the parser is not used anymore so it can handle web requests
+	castedPort, ok := strconv.Atoi(*engine.Flag_port)
+	if ok != nil {
+		panic("Provide port as integer.")
+	}
+	host, port, path, err := parseAddrPath(*engine.Flag_webUIHost, castedPort)
+	logUI.Log.PanicIfError(err)
+	go api.Start(host, port, path, *engine.Flag_tunnel, *engine.Flag_debug)
+
+	if *engine.Flag_interactive {
+		openbrowser("http://127.0.0.1:" + *engine.Flag_port)
+	}
+
+	// start executing the tree, possibly injecting commands from web gui
+	engine.EvalFile(code)
+
+	if *engine.Flag_interactive {
+		engine.RunInteractive()
+	}
 }
 
 func runScript(fname string) {
