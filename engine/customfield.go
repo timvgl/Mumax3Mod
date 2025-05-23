d29c24073715eeaeba865fc972d57ee18bc5624d
timvgl <tim.vogel@physik.uni-kl.de>
2025-05-23 16:25:58 +0200

    trying to fix index not found for buffer after some runtime

--- engine/customfield.go
+++ engine/customfield.go
@@ -441,7 +441,6 @@ func (q *masked) createMask() {
 	q.mask.Free()
 	q.mask = cuda.NewSlice(SCALAR, size)
 	data.Copy(q.mask, maskhost)
-	maskhost.Free()
 	q.mesh = *Mesh()
 	// Remove mask from host
 }
